2033811b342732731ff5a2c8b452693be731a242
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-06-22 22:07:35 +0530

    Create 169_majority_element.cpp

---
+++ Arrays/Easy/169_majority_element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int,int>hash;
+        for(int i=0; i<n; i++){
+            hash[nums[i]]++;
+        }
+        
+       for(auto it:hash){
+        if(it.second>n/2)return it.first;
+       }
+        
+        return -1;
+        
+            }
+};
