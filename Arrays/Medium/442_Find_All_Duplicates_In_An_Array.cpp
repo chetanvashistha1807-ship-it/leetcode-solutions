147c1cf73068fdc48afccefcc30015bc6c4595ed
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-06-23 21:16:01 +0530

    Update 442_Find_All_Duplicates_In_An_Array.cpp

--- Arrays/Medium/442_Find_All_Duplicates_In_An_Array.cpp
+++ Arrays/Medium/442_Find_All_Duplicates_In_An_Array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int>hash(nums.size()+1,0);
-        for(int i=1; i<nums.size(); i++){
+        for(int i=0; i<nums.size(); i++){
             hash[nums[i]]++;
         }
         vector<int>final;
-        for(int i=0; i<nums.size()+1; i++){
+        for(int i=1; i<nums.size()+1; i++){
          if(hash[i]>1)final.push_back(i);
         }
         return final;
