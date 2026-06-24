8e4c7b77b0f61bfddaab1a01fb635f3ad9b2dea2
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-06-24 23:48:41 +0530

    Update 75_Sort_Colors.cpp

--- Arrays/Easy/75_Sort_Colors.cpp
+++ Arrays/Easy/75_Sort_Colors.cpp
@@ -23,3 +23,31 @@ public:
         }
     }
 }; */
+
+
+//More Optimal solution in just one pass- Dutch National Flag Algorithm.
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){swap(nums[low],nums[mid]);
+            low++;
+            mid++;}
+           else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[high],nums[mid]);
+                high--;
+
+            }
+        }
+    }
+};
