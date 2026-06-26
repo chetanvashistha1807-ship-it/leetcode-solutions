da816c5610fb68052d3c0dc208e987a413f7efaf
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-06-26 17:02:10 +0530

    Create 31_Next_Permutation.cpp

---
+++ Arrays/Medium/31_Next_Permutation.cpp
@@ -0,0 +1,11 @@
+//Brute:- Use Recursion(Haven't studied it yet in depth)
+
+
+//Better: Using STL inbuilt function
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+        
+    }
+};
