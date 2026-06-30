4ef912d19ef55648912faddb55ef5bea057d7ee5
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-06-30 09:25:59 +0530

    Update 724_Find_Pivot_Index.cpp

--- Arrays/Easy/724_Find_Pivot_Index.cpp
+++ Arrays/Easy/724_Find_Pivot_Index.cpp
@@ -1,3 +1,5 @@
+// Brute force solution
+
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
@@ -17,6 +19,28 @@ public:
 
             if (left == right)
                 return i;
+        }
+
+        return -1;
+    }
+};
+
+//Optimal Solution
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftsum = 0;
+        int totalsum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            totalsum += nums[i];
+        }
+
+        for (int j = 0; j < nums.size(); j++) {
+            int rightsum = totalsum - leftsum - nums[j];
+            if (rightsum == leftsum)
+                return j;
+            leftsum += nums[j];
         }
 
         return -1;
