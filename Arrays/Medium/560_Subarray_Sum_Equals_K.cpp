21a25c84beafd4f11a00a14f1c1b7943f937bfee
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-07-03 22:08:45 +0530

    Create 560_Subarray_Sum_Equals_K.cpp

---
+++ Arrays/Medium/560_Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,21 @@
+//Brute Force Solution
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count =0;
+        int n =nums.size();
+
+        for(int i=0; i<n; i++){
+            int sum =nums[i];
+            for(int j=i; j<n; j++){
+                if(j==i)sum=nums[i];
+                else sum = sum+nums[j];
+                if(sum==k){count++;
+                           }
+
+            }
+        } return count;
+    }
+};
+
