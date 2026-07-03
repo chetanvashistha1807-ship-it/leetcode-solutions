773c640a77bcbed601aaa0898b763705d30e4d2a
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-07-03 17:02:00 +0530

    Create 48_Rotate_Image.cpp

---
+++ Arrays/Medium/48_Rotate_Image.cpp
@@ -0,0 +1,22 @@
+//Brute Force Solution
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = ans[i][j];
+            }
+        }
+    }
+};
+
