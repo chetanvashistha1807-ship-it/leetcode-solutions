486c8733a2c4ca7df016900b632258402b51abe1
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-07-03 17:34:16 +0530

    attempted 2 more questions

--- Arrays/Medium/48_Rotate_Image.cpp
+++ Arrays/Medium/48_Rotate_Image.cpp
@@ -20,3 +20,37 @@ public:
     }
 };
 
+//Optimal Solution
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+      
+       //Step 1 :- Transpose
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j > i) {
+                    swap(matrix[i][j], matrix[j][i]);
+                }
+            }
+        } 
+        //Step 2 : - Row Reversal
+
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n; j++){
+                if(j<n/2){
+                swap(matrix[i][j],matrix[i][n-j-1]);
+            }
+        }
+
+
+
+
+
+
+
+
+    }}
+    };
