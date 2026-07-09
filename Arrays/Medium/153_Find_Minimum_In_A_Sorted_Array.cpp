17bd774618e74eed73b244a51ad875fe2f082961
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-07-09 11:36:06 +0530

    Create 153_Find_Minimum_In_A_Sorted_Array.cpp

---
+++ Arrays/Medium/153_Find_Minimum_In_A_Sorted_Array.cpp
@@ -0,0 +1,17 @@
+//Brute Force Solution
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int mini = INT_MAX;
+
+        for(int i = 0; i<n; i++){
+            if(nums[i]<mini)mini=nums[i];
+        }
+        return mini;
+    }
+};
+
+//Optimal Solution - Using Binary Search
+
