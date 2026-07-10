5a2e273b1a9636fb40a2b594ce37b074e5f8016c
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-07-10 09:01:52 +0530

    Update 18_4_Sum.cpp

--- Arrays/Medium/18_4_Sum.cpp
+++ Arrays/Medium/18_4_Sum.cpp
@@ -32,5 +32,36 @@ public:
     }
 };
 
-// better solution
+// better solution : - using hash set
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> st;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                unordered_map<int, int> mp;
+
+                for(int k = j + 1; k < n; k++) {
+                    long long fourth = (long long)target - nums[i] - nums[j] - nums[k];
+
+                    if(fourth >= INT_MIN && fourth <= INT_MAX &&
+                       mp.find((int)fourth) != mp.end()) {
+
+                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+
+                    mp[nums[k]]++;
+                }
+            }
+        }
+
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
 
