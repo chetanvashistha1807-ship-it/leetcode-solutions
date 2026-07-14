b7c3e539588a0385f6acc9d97137ee7fc58ecfdf
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-07-14 22:28:27 +0530

    Create 56_Merge_Intervals.cpp

---
+++ Arrays/Medium/56_Merge_Intervals.cpp
@@ -0,0 +1,22 @@
+//I didn't understood shit abt this problem. Mayhaps I'll try this in future
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < intervals.size(); i++) {
+
+            if (ans.empty() || ans.back()[1] < intervals[i][0]) {
+                ans.push_back(intervals[i]);
+            }
+            else {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+        }
+
+        return ans;
+    }
+};
