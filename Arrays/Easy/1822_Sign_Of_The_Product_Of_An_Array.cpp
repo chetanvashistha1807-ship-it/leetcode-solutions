300a19bcde1863f4a928bfde959b0f38a11118fc
Chetan Vashistha <chetanvashistha1807@gmail.com>
2026-07-15 15:44:48 +0530

    Create 1822_Sign_Of_The_Product_Of_An_Array.cpp

---
+++ Arrays/Easy/1822_Sign_Of_The_Product_Of_An_Array.cpp
@@ -0,0 +1,24 @@
+//Good question
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int negatives = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                return 0;
+            }
+
+            if (nums[i] < 0) {
+                negatives++;
+            }
+        }
+
+        if (negatives % 2 == 0) {
+            return 1;
+        }
+
+        return -1;
+    }
+};
